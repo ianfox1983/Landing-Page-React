15e50b42582a521ec4bf2d33fa3ae48641c86879
ianfox1983 <ianfox1983@gmail.com>
2021-04-28 21:48:58 +0000

    Sin estilos Funciona

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import ModalFooter from "react-bootstrap/ModalFooter";
 
 export const Footer = () => {
 	return (
