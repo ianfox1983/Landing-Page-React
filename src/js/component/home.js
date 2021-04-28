9caa03a5962b6ce473296b1cee8564017366d3d8
Ivan Montecinos <76616196+ianfox1983@users.noreply.github.com>
2021-04-28 15:11:26 +0200

    Update home.js

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,33 +7,15 @@ import{Nav} from "./Nav";
 
 
 //create your first component
-export function Nav(){
-let menu = [
-    { label: "Home" },
-    { label: "Videos" },
-    { label: "Images" },
-    { label: "VideoGames" },
-];
-    return menu;
-}
-export function Home() {
-    let navbar = "Estoseria la navbar";
-    let titulo = <h1>Hola esta es una prueba </h1>;
+
+export function Home = () => {
+    
     return (
-        <div className="text-center mt-5">
-            <h1>Hello Rigo!</h1>
-            <p>
-                <img src={rigoImage} />
-            </p>
-            <a href="#" className="btn btn-success">
-                If you see this green button... bootstrap is working
-			</a>
-            <p>
-                Made by{" "}
-                <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-        </div>
-    );
-}
+	    <Navbar
+		let navbar = "Estoseria la navbar";
+		let titulo = <h1>Hola esta es una prueba </h1>;
+	    />
+	 );
+       
+};
 
